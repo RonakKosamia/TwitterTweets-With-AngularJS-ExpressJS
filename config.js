d8a3b58bfab8bd809ec0127c07e88f6e5be92f8c
RonakKosamia <kosamiar@yahoo.com>
2017-01-05 12:11:58 -0500

    First commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: 'QlYOGY4Off4jMcPYG1FYd5dBi',
+		consumer_secret: 'h316scPm7VxWk0Tp5yUwxO0q0TSw5OTmE72ZXeN72GOr2I8tey',
+		access_token: '2889000714-rzDeVe2ebJcfdoovRyAdqpGjbKXeSinK80l9bZM',
+		access_token_secret: 'YXcRdDhwUKRqxsrUrVPJdun2zENy7ZNLf8NJ5O0UHxXig'
+	}
+
+};
